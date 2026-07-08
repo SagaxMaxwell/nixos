f84a6d18aa7ae8975530e2ebabe020a1f73194ee
Maxwell <sagax.maxwell@gmail.com>
2026-07-08 10:19:48 +0000

    hello

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,6 +9,9 @@ config.color_scheme = "OneHalfDark"
 -- Prefer GPU rendering for better performance (macOS)
 config.front_end = "WebGpu"
 
+-- Work around GNOME Wayland window dragging issues by using XWayland.
+config.enable_wayland = false
+
 -- Disable tab bar for slightly lower overhead
 config.enable_tab_bar = false
 
